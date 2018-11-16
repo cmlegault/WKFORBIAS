fecb622c3b86896b707ae69be33ca8b8227f0966
cmlegault <chris.legault@noaa.gov>
2018-11-16 08:11:54 -0500

    add calc Agg B and tests

--- tests/testthat/test_utilities.R
+++ tests/testthat/test_utilities.R
@@ -15,3 +15,16 @@ test_that("Lognormal Error input randomval", {
 test_that("Lognormal Error bias adjustment", {
   expect_equal(addLognormalError(1, 1, biasadjustflag = TRUE, randomval = 0), exp(-0.5))
 })
+
+# calcAggregateBiomass
+test_that("calc Agg B NAA NA", {
+  expect_equal(is.na(calcAggregateBiomass(NA, 0.3)), TRUE)
+})
+
+test_that("calc Agg B summation", {
+  expect_equal(calcAggregateBiomass(rep(100, 3), rep(0.2, 3)), 60)
+})
+
+test_that("calc Agg B indextime", {
+  expect_equal(calcAggregateBiomass(rep(100, 3), rep(1, 3), 0, 0, 0.5), 300)
+})
