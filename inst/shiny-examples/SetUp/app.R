ad934e5a127cae5051088aa0ffc2732cd109faf8
cmlegault <chris.legault@noaa.gov>
2018-11-19 10:48:11 -0500

    reformat ui indenting and improve dimplot

--- inst/shiny-examples/SetUp/app.R
+++ inst/shiny-examples/SetUp/app.R
@@ -9,79 +9,88 @@
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(strong("WKFORBIAS Set Up"),
+     
+  tabPanel("Dimensions",
+    sidebarLayout(
+     sidebarPanel(
+       selectInput("year1",
+                   "First Year",
+                   choices = c(1900:2020),
+                   selected = 1991),
+       
+       sliderInput("nyears",
+                   "Number of Years",
+                   min = 5,
+                   max = 100,
+                   value = 20),
+       
+       sliderInput("nages",
+                   "Number of Ages",
+                   min = 4,
+                   max = 50,
+                   value = 10),
+       
+       checkboxInput("plusgroupflag",
+                     "Last Age a Plus Group?",
+                     value = TRUE),
+       
+       sliderInput("nindices",
+                   "Number of Indices",
+                   min = 1,
+                   max = 20,
+                   value = 2)
+     ),
+     mainPanel(
+       plotOutput("dimPlot")
+     )
+    )
+  ),
   
-   tabPanel("Dimensions",
-     sidebarLayout(
-       sidebarPanel(
-         selectInput("year1",
-                     "First Year",
-                     choices = c(1900:2020),
-                     selected = 1991),
-         
-         sliderInput("nyears",
-                     "Number of Years",
-                     min = 5,
-                     max = 100,
-                     value = 20),
-         
-         sliderInput("nages",
-                     "Number of Ages",
-                     min = 4,
-                     max = 50,
-                     value = 10),
-         
-         checkboxInput("plusgroupflag",
-                       "Last Age a Plus Group?",
-                       value = TRUE),
-         
-         sliderInput("nindices",
-                     "Number of Indices",
-                     min = 1,
-                     max = 20,
-                     value = 2)
-       ),
-       mainPanel(
-         plotOutput("dimPlot")
-       )
+  tabPanel("M",
+    sidebarLayout(
+     sidebarPanel(
+     selectInput("Mopt",
+                 "Natural Mortality",
+                 choices = list("User Input", "Single Value", "Constant by Age", "More"),
+                 selected = "Single Value")
+     ),
+     mainPanel(
+       plotOutput("Mplot")
      )
-   ),
-   
-   tabPanel("M",
-     sidebarLayout(
-       sidebarPanel(
-         selectInput("Mopt",
-                     "Natural Mortality",
-                     choices = list("User Input", "Single Value", "Constant by Age", "More"),
-                     selected = "Single Value")
-       ),
-       mainPanel(
-         plotOutput("Mplot")
-       )
+    )
+  ),
+  
+  tabPanel("Nyear1",
+    sidebarLayout(
+     sidebarPanel(
+     selectInput("Nyear1opt",
+                 "Population numbers at age in first year",
+                 choices = list("User Input", "Equilibrium", "Equilibrium with Noise"),
+                 selected = "Equilibrium")
+     ),
+     mainPanel(
+       plotOutput("Nyear1plot")
      )
-   ),
-
-   tabPanel("Nyear1",
-     sidebarLayout(
-       sidebarPanel(
-         selectInput("Nyear1opt",
-                     "Population numbers at age in first year",
-                     choices = list("User Input", "Equilibrium", "Equilibrium with Noise"),
-                     selected = "Equilibrium")
-       ),
-       mainPanel(
-         plotOutput("Nyear1plot")
-       )
-     )
-   )
-
+    )
+  )
+  
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$dimPlot <- renderPlot({
-     yr1 <- as.numeric(input$year1)
-     plot(1:10,1:10,xlim=c(1, input$nages), ylim=c(yr1, yr1 + input$nyears - 1), type='n', xlab="Age", ylab="Year")
+     
+  years <- reactive({
+    yr1 <- as.numeric(input$year1)
+    seq(yr1, yr1 + input$nyears - 1)
+  })  
+  
+  ages <- reactive({
+    seq(1, input$nages)
+  })
+  
+  output$dimPlot <- renderPlot({
+    ya <- expand.grid(Age = ages(), Year = years())
+    plot(ya$Age, ya$Year, xlab="Age", ylab="Year")
      title(paste("Number of Indices =", input$nindices))
    })
    
