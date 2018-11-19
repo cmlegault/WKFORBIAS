2b2a6cbb43e65f719d644e6c8261b6e1900c5f9c
cmlegault <chris.legault@noaa.gov>
2018-11-19 15:02:56 -0500

    change from flag to list structure

--- R/createOM.R
+++ R/createOM.R
@@ -4,33 +4,33 @@
 #' @param years vector of years in operating model (OM)
 #' @param nages number of ages in OM (assumed to start at age 1)
 #' @param nindices number of indices
-#' @param nyear1flag N in first year option
-#' @param Rflag recruitment option
-#' @param Mflag natural mortality option
-#' @param Fflag fishing mortality option
-#' @param Wflag weights at age option
-#' @param indexflag list of index options
-#' @param catcherrorflag option for observation errors in catch
-#' @param indexerrorflag list of options for observation errors in the indices
-#' @param processerrorflag option for process error in survival equation, default=FALSE
+#' @param nyear1list N in first year list
+#' @param Rlist recruitment list
+#' @param Mlist natural mortality list
+#' @param Flist fishing mortality list
+#' @param Wlist weights at age list
+#' @param indexlist list of index lists
+#' @param catcherrorlist list for observation errors in catch
+#' @param indexerrorlist list of lists for observation errors in the indices
+#' @param processerrorlist list for process error in survival equation, default=FALSE
 #' @export
 
-createOM <- function(years, nages, nindices, nyear1flag, Rflag, Mflag, Fflag, Wflag, indexflag, catcherrorflag, indexerrorflag, processerrorflag=FALSE){
+createOM <- function(years, nages, nindices, nyear1list, Rlist, Mlist, Flist, Wlist, indexlist, catcherrorlist, indexerrorlist, processerrorlist=FALSE){
   om <- list()
   om$inputs <- list()
   # save the input values
   om$inputs$years <- years
   om$inputs$nages <- nages
   om$inputs$nindices <- nindices
-  om$inputs$nyear1flag <- nyear1flag
-  om$inputs$Rflag <- Rflag
-  om$inputs$Mflag <- Mflag
-  om$inputs$Fflag <- Fflag
-  om$inputs$Wflag <- Wflag
-  om$inputs$indexflag <- indexflag
-  om$inputs$catcherrorflag <- catcherrorflag
-  om$inputs$indexerrorflag <- indexerrorflag
-  om$inputs$processerrorflag <- processerrorflag
+  om$inputs$nyear1list <- nyear1list
+  om$inputs$Rlist <- Rlist
+  om$inputs$Mlist <- Mlist
+  om$inputs$Flist <- Flist
+  om$inputs$Wlist <- Wlist
+  om$inputs$indexlist <- indexlist
+  om$inputs$catcherrorlist <- catcherrorlist
+  om$inputs$indexerrorlist <- indexerrorlist
+  om$inputs$processerrorlist <- processerrorlist
   
   # create blank matrices
   nyears <- length(years)
