3f0bb833d323a80b588fa0dd43800072c22054f9
cmlegault <chris.legault@noaa.gov>
2018-11-19 10:00:22 -0500

    add Nyear1 to Shiny

--- inst/shiny-examples/SetUp/app.R
+++ inst/shiny-examples/SetUp/app.R
@@ -7,28 +7,52 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
+ui <- navbarPage(strong("WKFORBIAS Set Up"),
+  
+   tabPanel("Dimensions",
+     sidebarLayout(
+       sidebarPanel(
+         sliderInput("years",
+                     "Years",
+                     min = 1960,
+                     max = 2050,
+                     value = c(1991, 2018)),
+         
+         sliderInput("nages",
+                     "Number of Ages",
+                     min = 4,
                      max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
+                     value = 10),
+         
+         checkboxInput("plusgroupflag",
+                       "Last Age a Plus Group?",
+                       value = TRUE),
+         
+         sliderInput("nindices",
+                     "Number of Indices",
+                     min = 1,
+                     max = 20,
+                     value = 2)
+       ),
+       mainPanel(
          plotOutput("distPlot")
-      )
+       )
+     )
+   ),
+   
+   tabPanel("Nyear1",
+     sidebarLayout(
+       sidebarPanel(
+         selectInput("Nyear1opt",
+                     "Population numbers at age in first year",
+                     choices = list("User Input", "Equilibrium", "Equilibrium with Noise"),
+                     selected = "Equilibrium")
+       ),
+       mainPanel(
+         plotOutput("Nyear1plot")
+       )
+     )
    )
 )
 
@@ -38,10 +62,22 @@ server <- function(input, output) {
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
       x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      bins <- seq(min(x), max(x), length.out = input$nages + 1)
       
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+   
+   output$Nyear1plot <- renderPlot({
+     Nyear1 <- rep(1000, input$nages)
+     zaa <- 0.5
+     for (iage in 2:input$nages){
+       Nyear1[iage] <- Nyear1[iage - 1] * exp(-zaa)
+     }
+     if (input$plusgroupflag == TRUE){
+       Nyear1[input$nages] <- Nyear1[input$nages] / (1 - exp(-zaa))
+     }
+     plot(1:input$nages, Nyear1, type='o')
    })
 }
 
