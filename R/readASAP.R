8efc056145c521c5ce59571e9299ff576f6e61ca
cmlegault <chris.legault@noaa.gov>
2018-12-11 14:40:20 -0500

    more readASAP parts completed

--- R/readASAP.R
+++ R/readASAP.R
@@ -24,6 +24,57 @@ readASAP <- function(asap.name, wd){
   OM$readASAP$asap.name <- asap.name
   OM$readASAP$wd <- wd
   
+  OM$years <- seq(asap$parms$styr, asap$parms$endyr)
+  OM$nyears <- length(OM$years)
+  OM$nages <- asap$parms$nages
+  OM$nindices <- asap$parms$nindices
   
+  OM$nyear1list <- list()
+  OM$nyear1list$type <- "constant"
+  OM$nyear1list$values <- asap$N.age[1, ]
+  
+  OM$Rlist <- list()
+  OM$Rlist$type <- "constant"
+  OM$Rlist$values <- asap$N.age[, 1]
+  
+  OM$Mlist <- list()
+  OM$Mlist$type <- "constant"
+  OM$Mlist$values <- asap$M.age
+  
+  OM$Flist <- list()
+  OM$Flist$type <- "constant"
+  OM$Flist$values <- asap$F.age
+  
+  OM$Wlist <- list()
+  OM$Wlist$type <- "constant"
+  OM$Wlist$values <- asap$WAA.mats$WAA.catch.all # note: doesn't handle discards
+  
+  OM$indexlist <- list()
+  for (ind in 1:OM$nindices){
+    OM$indexlist[[ind]] <- list()
+    OM$indexlist[[ind]]$selxtype <- "constant"
+    OM$indexlist[[ind]]$selxvalues <- asap$index.sel[ind, ]
+    OM$indexlist[[ind]]$qtype <- "constant"
+    OM$indexlist[[ind]]$qvalues <- asap$q.indices[ind]
+  }
+  
+  OM$catcherrorlist <- list()
+  OM$catcherrorlist$type <- "observation error only"
+  OM$catcherrorlist$CVvalues <- as.numeric(asap$control.parms$catch.tot.cv)  # assumes only one fleet
+  OM$catcherrorlist$ESSvalues <- as.numeric(asap$fleet.catch.Neff.init)
+  
+  OM$indexerrorlist <- list()
+  OM$indexerrorlist$type <- "observation error only"
+  for (ind in 1:OM$nindices){
+    OM$indexerrorlist[[ind]] <- list()
+    OM$indexerrorlist[[ind]]$CVvalues <- asap$index.cv[[ind]] # assumes all years have index
+    OM$indexerrorlist[[ind]]$ESSvalues <- asap$index.Neff.init[ind, ]
+  }
+  
+  OM$processerrorlist <- FALSE
+
   return(OM)
 }
+
+# example call
+# readASAP("simple", "C:\\Users\\chris.legault\\Desktop\\testplots")
