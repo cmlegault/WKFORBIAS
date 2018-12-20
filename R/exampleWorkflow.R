8e7db901da654b38ba38700167242d74f010184d
cmlegault <chris.legault@noaa.gov>
2018-12-20 13:50:40 -0500

    improve workflow example

--- R/exampleWorkflow.R
+++ R/exampleWorkflow.R
@@ -1,12 +1,18 @@
 # example workflow
 # just a placeholder for testing
 # convert to vignette later
+# don't forget to Build>Install and Restart first
+# also good to run devtools::test() to make sure everything still working
+# have to comment code to get it to compile in package, just uncomment block below and run
 
-# get data from an ASAP run
+## uncomment starting here ##
+
+# # get data from an ASAP run
 # myom <- readASAP("simple", "C:\\Users\\chris.legault\\Desktop\\testplots")
 # names(myom)
-
-# fill in true values
+# 
+# # fill in true values
 # myomTrue <- fillTrueOM(myom)
 # names(myomTrue)
 # myomTrue$NAA
+
