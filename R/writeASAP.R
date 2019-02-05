03b44b3ddf8fafdbee0c5faca6bbcadac722cbcd
cmlegault <chris.legault@noaa.gov>
2019-02-05 12:40:55 -0500

    start of writeASAP function

---
+++ R/writeASAP.R
@@ -0,0 +1,15 @@
+#' Write ASAP data file 
+#' 
+#' Writes an ASAP input based on operating model OM.
+#' @param om operating model list containing data
+#' @param ASAPoptions list of options to use in the ASAP input file
+#' @param ASAPinputFileName name for ASAP input file
+#' @param wd working directory to write ASAP input file to
+#' @export
+
+writeASAP <- function(om, ASAPoptions, ASAPinputFileName, wd){
+  filewriteOK <- FALSE
+  
+  # add filewriteOK <- TRUE when ASAP input file successfully written
+  return(filewriteOK)
+}
