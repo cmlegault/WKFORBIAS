8735d87a17f3aa5d391808bd8c043eb7ef13d506
cmlegault <chris.legault@noaa.gov>
2019-05-03 15:18:26 -0400

    explain how to use function

--- R/createControlObs.R
+++ R/createControlObs.R
@@ -1,5 +1,6 @@
-#' Create control list for observations (determines error structures, CVs, samples sizes, etc.)
-#' 
+#' Create control list for observations
+#'  
+#' Determines error structures, CVs, samples sizes, etc., defaults set so can just change one or more values as needed
 #' @param ESSCAA effective sample size for multinomial distribution of catch at age (default=100)
 #' @param CtotCV coefficient of variation for total catch in weight (default=0.05)
 #' @export
